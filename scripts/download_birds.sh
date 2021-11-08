dec85c920d1d40222ab11f5125bf7fa00be2c884
Douglas Souza <douglas.souza.002@acad.pucrs.br>
2021-11-08 16:49:09 -0300

    Update birds dataset URLs

--- scripts/download_birds.sh
+++ scripts/download_birds.sh
@@ -1,10 +1,15 @@
-mkdir datasets
+OUT_DIR='datasets/CUB'
+mkdir -p $OUT_DIR
 echo 'Downloading birds... about 4GB'
-gdown https://drive.google.com/uc?id=1fCspnLiKAjOe7JKqAH8KDbcvt4FS__g6
-tar -xzf birds.tar.gz -C datasets/
-rm birds.tar.gz
+# download images
+gdown https://drive.google.com/uc?id=13w8kckfJTWb4LD0Xf4J0huvgWmFLKpb9
+tar -xzf images.tar.gz -C $OUT_DIR
+rm images.tar.gz
+# download captions
+gdown https://drive.google.com/uc?id=1hZXdCetIVsWR_LyL_rcQzP_NLmSu_Xcs --output $OUT_DIR/train/
+gdown https://drive.google.com/uc?id=1xyh3EKe372xJgJXkShN73fHgShspM0Ca --output $OUT_DIR/test/
 
-echo 'Downloading inception model birds... 362MB'
+# echo 'Downloading inception model birds... 362MB'
 gdown https://drive.google.com/uc?id=1Fn6JgALnESsIxPw34-s2saHtA1gc4bfF
 tar -xzf inception_finetuned_models.tar.gz -C evaluation/inception_score
 rm inception_finetuned_models.tar.gz
